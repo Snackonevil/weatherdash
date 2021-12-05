17572b3c3dd370813bd5492bbc953b463df52875
Kevin Lacson <lacsonky@gmail.com>
2021-12-05 17:59:45 -0500

    added bootstrap theme

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,7 @@
 // const milliseconds = 1575909015 * 1000 // 1575909015000
 // const dateObject = new Date(milliseconds)
 // const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
+
 var searchInput = $("#searchInput");
 const searchBtn = $("#searchBtn");
 const mainContent = $("#mainContent");
@@ -22,14 +23,14 @@ const apiRootUrl = "https://api.openweathermap.org/";
 const apiKey = "19f6c11012fcd19bbfc2f0188a37308e";
 var city = "atlanta";
 
-let writeData = (data) => {
-  var currTemp = data.current;
-  console.log(currTemp);
-  for (let i = 0; i < 5; i++) {
-    var day = data.daily;
-    console.log(day[i].temp);
-  }
-  console.log(data);
+let writeData = data => {
+    var currTemp = data.current;
+    console.log(currTemp);
+    for (let i = 0; i < 5; i++) {
+        var day = data.daily;
+        console.log(day[i].temp);
+    }
+    console.log(data);
 };
 
 // let fetchFromZip = (zip) => {
@@ -46,45 +47,45 @@ let writeData = (data) => {
 //     });
 // };
 
-// One Call
-let fetchWeather = (coordObj) => {
-  fetch(
-    `${apiRootUrl}data/2.5/onecall?lat=${coordObj.lat}&lon=${coordObj.lon}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      writeData(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+// OneCall Weather
+let fetchWeather = coordObj => {
+    fetch(
+        `${apiRootUrl}data/2.5/onecall?lat=${coordObj.lat}&lon=${coordObj.lon}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`
+    )
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            writeData(data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 };
 
 // Geocode
-let fetchCoords = (city) => {
-  fetch(`${apiRootUrl}data/2.5/forecast?q=${city}&appid=${apiKey}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      fetchWeather(data.city.coord);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+let fetchCoords = city => {
+    fetch(`${apiRootUrl}data/2.5/forecast?q=${city}&appid=${apiKey}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            fetchWeather(data.city.coord);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 };
 
-searchBtn.click((e) => {
-  e.preventDefault();
-  //   console.log(typeof searchInput.val());
-  //   if (typeof searchInput.val() == "number") {
-  //     var zip = searchInput.val();
-  //     fetchFromZip(zip);
-  //   } else {
-  // console.log(searchInput.val());
-  var city = searchInput.val().replace(" ", "+");
-  fetchCoords(city);
+searchBtn.click(e => {
+    e.preventDefault();
+    //   console.log(typeof searchInput.val());
+    //   if (typeof searchInput.val() == "number") {
+    //     var zip = searchInput.val();
+    //     fetchFromZip(zip);
+    //   } else {
+    // console.log(searchInput.val());
+    var city = searchInput.val().replace(" ", "+");
+    fetchCoords(city);
 });
