4cd617bde8525a1219dd72e10f8818182aa88351
Kevin Lacson <lacsonky@gmail.com>
2021-12-10 03:53:55 -0500

    more refactoring

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,52 @@
 // Selectors
-var searchInput = $("#searchInput");
-const searchBtn = $("#searchBtn");
-const mainContent = $("#mainContent");
-var recentSearches = $("#recents");
-const current = $("#current");
-const fiveDay = $("#five-day");
-const clearBtn = $("#clearBtn");
+var searchInput = $('#searchInput');
+const searchBtn = $('#searchBtn');
+const mainContent = $('#mainContent');
+var recentSearches = $('#recents');
+const current = $('#current');
+const fiveDay = $('#five-day');
+const clearBtn = $('#clearBtn');
 
 // Global Variables
-const apiRootUrl = "https://api.openweathermap.org/";
-const apiKey = "9eae40a4431a1836c424f06650dd3e9d";
+const apiRootUrl = 'https://api.openweathermap.org/';
+const apiKey = '9eae40a4431a1836c424f06650dd3e9d';
 var searchHistory = [];
 // 9eae40a4431a1836c424f06650dd3e9d
 // "655d8562d037c93897e1a3c13e7c403b"
 
-let handleHistory = (city) => {
-  var searchHistory = JSON.parse(localStorage.getItem("history"));
+let handleHistory = city => {
+  var searchHistory = JSON.parse(localStorage.getItem('history'));
   if (searchHistory == null) {
     searchHistory = [city]; // if no previous history
-    console.log("New history");
+    console.log('New history');
   } else if (searchHistory.indexOf(city) === -1) {
     searchHistory.unshift(city); // if city has not yet been searched
-    console.log("New city...");
+    console.log('New city...');
   } else {
     searchHistory.splice(searchHistory.indexOf(city), 1);
-    searchHistory.unshift(city); // if city has been search, put to front
-    console.log("Previous city");
+    searchHistory.unshift(city); // if city has been searched, put at 0 index
+    console.log('Previous city');
   }
-  localStorage.setItem("history", JSON.stringify(searchHistory)); // reset updated history
+  localStorage.setItem('history', JSON.stringify(searchHistory)); // reset updated history
   loadHistory();
-  console.log("loading history...");
+  console.log('loading history...');
 };
 
 // Render history in dropdown
 let loadHistory = () => {
-  var searchHistory = JSON.parse(localStorage.getItem("history"));
+  var searchHistory = JSON.parse(localStorage.getItem('history'));
   if (searchHistory == null) {
     return;
   }
-  var output = "";
-  searchHistory.forEach((i) => {
+  var output = '';
+  searchHistory.forEach(i => {
     output += `<li><a class="dropdown-item" href="#">${i}</a></li>`;
   });
   recentSearches.html(output);
 };
 
 // Render weather forecast card
-let writeData = (data) => {
+let writeData = data => {
   var curr = data.current;
   // Render current weather
   current.html(`
@@ -60,50 +60,50 @@ let writeData = (data) => {
   }@4x.png" />`);
 
   // Render five-day forecast
-  var output = "";
+  var output = '';
   for (let i = 1; i < 6; i++) {
     var day = data.daily;
-    var temp = Math.floor(day[i].temp.max); // Round temp to whole number
+    var temp = Math.floor(day[i].temp.max);
     // Reformat date from Unix
     var weatherInfo = day[i].weather[0];
     var unixMillisec = day[i].dt * 1000;
     var dateObject = new Date(unixMillisec);
-    var date = dateObject.toLocaleString("en-US", {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
+    var date = dateObject.toLocaleString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
     });
-
     output += `<div class="col p-3 m-1 border rounded text-center">${date}<img src="http://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png"/><h4>${temp}</h4><p>${weatherInfo.description}</p></div>`;
   }
-  console.log("weather loaded");
+  fiveDay.html(output);
+  console.log('weather loaded');
 };
 
 // OneCall Weather
-let fetchWeather = (coordObj) => {
+let fetchWeather = coordObj => {
   fetch(
     `${apiRootUrl}data/2.5/onecall?lat=${coordObj.lat}&lon=${coordObj.lon}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`
   )
-    .then((response) => {
+    .then(response => {
       // if (!response.ok) {
       //     return;
       // }
       return response.json();
     })
-    .then((data) => {
+    .then(data => {
       writeData(data);
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 };
 
 // Geocode
-let fetchCoords = (city) => {
+let fetchCoords = city => {
   fetch(`${apiRootUrl}data/2.5/forecast?q=${city}&appid=${apiKey}`)
-    .then((response) => {
+    .then(response => {
       if (response.status === 404) {
-        $("#mainContent section h1").text(`${city} not found`);
+        $('#mainContent section h1').text(`${city} not found`);
         return;
       }
       // } else if (response.status === 429) {
@@ -113,27 +113,27 @@ let fetchCoords = (city) => {
       // }
       return response.json();
     })
-    .then((data) => {
+    .then(data => {
       // console.log(data);
-      $("#mainContent section h1").text(
+      $('#mainContent section h1').text(
         `Today in ${data.city.name}, ${data.city.country}`
       );
-      fetchWeather(data.city.coord); // get weather with coordinates
-      handleHistory(city.replace("+", " ")); // process city name into history
+      fetchWeather(data.city.coord); // fetch weather with coordinates
+      handleHistory(city.replace('+', ' ')); // process city name into history
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 };
 
 // Load page based on history
 let pageLoad = () => {
-  var searchHistory = JSON.parse(localStorage.getItem("history"));
+  var searchHistory = JSON.parse(localStorage.getItem('history'));
   if (searchHistory == null) {
     return;
   }
-  var output = "";
-  searchHistory.forEach((i) => {
+  var output = '';
+  searchHistory.forEach(i => {
     output += `<li><a class="dropdown-item" href="#">${i}</a></li>`;
   });
   recentSearches.html(output);
@@ -143,25 +143,25 @@ let pageLoad = () => {
 //Events
 
 // Search for user input
-searchBtn.click((e) => {
-  if (searchInput.val() === "") {
+searchBtn.click(e => {
+  if (searchInput.val() === '') {
     return;
   }
   e.preventDefault();
-  var city = searchInput.val().replace(" ", "+");
+  var city = searchInput.val().replace(' ', '+');
   fetchCoords(city);
-  searchInput.val(""); // Empty input field
+  searchInput.val(''); // Empty input field
 });
 
 // Recall city from history dropdown
-recentSearches.click((e) => {
+recentSearches.click(e => {
   fetchCoords($(e.target).text());
 });
 
 // Clear 'history' key from storage
-clearBtn.click((e) => {
+clearBtn.click(e => {
   e.preventDefault();
-  localStorage.removeItem("history");
+  localStorage.removeItem('history');
   loadHistory();
 });
 
