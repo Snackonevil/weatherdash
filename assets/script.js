cd1e919a24a0fd926e97f53e67774a6f4d8055b0
Kevin Lacson <lacsonky@gmail.com>
2021-12-10 03:44:30 -0500

    added comments and removed console.logs for development

--- assets/script.js
+++ assets/script.js
@@ -17,21 +17,22 @@ var searchHistory = [];
 let handleHistory = (city) => {
   var searchHistory = JSON.parse(localStorage.getItem("history"));
   if (searchHistory == null) {
-    searchHistory = [city];
+    searchHistory = [city]; // if no previous history
     console.log("New history");
   } else if (searchHistory.indexOf(city) === -1) {
-    searchHistory.unshift(city);
+    searchHistory.unshift(city); // if city has not yet been searched
     console.log("New city...");
   } else {
     searchHistory.splice(searchHistory.indexOf(city), 1);
-    searchHistory.unshift(city);
+    searchHistory.unshift(city); // if city has been search, put to front
     console.log("Previous city");
   }
-  localStorage.setItem("history", JSON.stringify(searchHistory));
+  localStorage.setItem("history", JSON.stringify(searchHistory)); // reset updated history
   loadHistory();
   console.log("loading history...");
 };
 
+// Render history in dropdown
 let loadHistory = () => {
   var searchHistory = JSON.parse(localStorage.getItem("history"));
   if (searchHistory == null) {
@@ -44,9 +45,10 @@ let loadHistory = () => {
   recentSearches.html(output);
 };
 
+// Render weather forecast card
 let writeData = (data) => {
   var curr = data.current;
-  console.log(curr);
+  // Render current weather
   current.html(`
     <h3>Temperature: ${Math.floor(curr.temp)} | Feels like: ${Math.floor(
     curr.feels_like
@@ -57,10 +59,11 @@ let writeData = (data) => {
     curr.weather[0].icon
   }@4x.png" />`);
 
+  // Render five-day forecast
   var output = "";
   for (let i = 1; i < 6; i++) {
     var day = data.daily;
-    var temp = Math.floor(day[i].temp.max);
+    var temp = Math.floor(day[i].temp.max); // Round temp to whole number
     // Reformat date from Unix
     var weatherInfo = day[i].weather[0];
     var unixMillisec = day[i].dt * 1000;
@@ -72,10 +75,8 @@ let writeData = (data) => {
     });
 
     output += `<div class="col p-3 m-1 border rounded text-center">${date}<img src="http://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png"/><h4>${temp}</h4><p>${weatherInfo.description}</p></div>`;
-    // console.log(day[i].temp.max);
   }
-  //   fiveDay.html(output);
-  //   console.log(data);
+  console.log("weather loaded");
 };
 
 // OneCall Weather
@@ -117,14 +118,15 @@ let fetchCoords = (city) => {
       $("#mainContent section h1").text(
         `Today in ${data.city.name}, ${data.city.country}`
       );
-      fetchWeather(data.city.coord);
-      handleHistory(city.replace("+", " "));
+      fetchWeather(data.city.coord); // get weather with coordinates
+      handleHistory(city.replace("+", " ")); // process city name into history
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
+// Load page based on history
 let pageLoad = () => {
   var searchHistory = JSON.parse(localStorage.getItem("history"));
   if (searchHistory == null) {
@@ -138,6 +140,9 @@ let pageLoad = () => {
   fetchCoords(searchHistory[0]);
 };
 
+//Events
+
+// Search for user input
 searchBtn.click((e) => {
   if (searchInput.val() === "") {
     return;
@@ -145,16 +150,18 @@ searchBtn.click((e) => {
   e.preventDefault();
   var city = searchInput.val().replace(" ", "+");
   fetchCoords(city);
-  searchInput.val("");
+  searchInput.val(""); // Empty input field
 });
 
+// Recall city from history dropdown
 recentSearches.click((e) => {
   fetchCoords($(e.target).text());
 });
 
+// Clear 'history' key from storage
 clearBtn.click((e) => {
   e.preventDefault();
-  localStorage.clear();
+  localStorage.removeItem("history");
   loadHistory();
 });
 
